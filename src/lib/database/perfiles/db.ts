af59ac3cbbe8ea87a1530c516a361de45ce665cb
Ivan <ivangyr321@gmail.com>
2025-09-27 12:45:29 -0500

    Agregar módulo PerfilDB para gestionar la creación y lectura de perfiles y roles de usuario

---
+++ src/lib/database/perfiles/db.ts
@@ -0,0 +1,40 @@
+import type { SupabaseClient } from '@supabase/supabase-js';
+import type { Perfil, PerfilRol } from './type';
+
+export const PerfilDB = {
+	async createPerfil(supabase: SupabaseClient, perfil: Perfil) {
+		const { data, error } = await supabase.from('perfiles').insert(perfil);
+
+		if (error) {
+			throw new Error('Error al crear el perfil: ' + error.message);
+		}
+
+		return data;
+	},
+
+	async readPerfilById(supabase: SupabaseClient, idPerfil: string) {
+		const { data, error } = await supabase
+			.from('perfiles')
+			.select('*')
+			.eq('id_user', idPerfil)
+			.single();
+
+		if (error) {
+			throw new Error('Error al leer el perfil: ' + error.message);
+		}
+
+		return data;
+	},
+
+	async readRolByIdUsuario(supabase: SupabaseClient, id_usuario: string) {
+		const { data, error } = await supabase
+			.from('perfiles')
+			.select('rol')
+			.eq('id_user', id_usuario)
+			.single();
+		if (error) {
+			throw new Error('Error al leer el rol del usuario: ' + error.message);
+		}
+		return data?.rol as PerfilRol;
+	}
+};
