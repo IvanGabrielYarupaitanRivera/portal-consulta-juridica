143b5016dd3f663da2bc43a3218ececec446ee21
Ivan <ivangyr321@gmail.com>
2025-09-28 01:16:57 -0500

    Agregar lógica para crear consultas y manejar cierre de sesión

---
+++ src/routes/actions/users/+page.server.ts
@@ -0,0 +1,48 @@
+import { ConsultaDB } from '$lib/database/consultas/db';
+import type { Consulta } from '$lib/database/consultas/type';
+import { UserDB } from '$lib/database/users/db';
+import type { Actions } from '@sveltejs/kit';
+import { fail } from 'assert';
+
+export const actions = {
+	createConsulta: async ({ request, locals }) => {
+		const { supabase } = locals;
+		const formData = await request.formData();
+
+		const consulta: Consulta = {
+			id_consulta: crypto.randomUUID(),
+			nombre: formData.get('nombre') as string,
+			whatsapp: formData.get('whatsapp') as string | null,
+			mensaje: formData.get('mensaje') as string | null,
+			respuesta: 'Sin respuesta',
+			estado: 'Pendiente',
+			id_abogado: null,
+			created_at: new Date().toISOString(),
+			updated_at: new Date().toISOString(),
+			responded_at: null
+		};
+
+		try {
+			await ConsultaDB.createConsulta(supabase, consulta);
+			return { success: true, message: 'Su consulta ha sido registrada exitosamente.' };
+		} catch (err) {
+			return fail(500, {
+				success: false,
+				message: err instanceof Error ? err.message : 'Error al crear la consulta.'
+			});
+		}
+	},
+
+	logout: async ({ locals: { supabase } }) => {
+		console.log('Logging out...');
+		try {
+			await UserDB.signOut(supabase);
+			return { success: true, message: 'Sesión cerrada' };
+		} catch (err) {
+			return fail(500, {
+				success: false,
+				message: err instanceof Error ? err.message : 'Error en el servidor'
+			});
+		}
+	}
+} satisfies Actions;
