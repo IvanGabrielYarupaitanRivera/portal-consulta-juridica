134b32b4d746c1f9811339596ccadb3a3462319b
Ivan <ivangyr321@gmail.com>
2025-09-29 23:10:16 -0500

    Agregar manejo de rol de usuario en la carga del layout

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,13 @@
+import { PerfilDB } from '$lib/database/perfiles/db';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async ({ locals: { safeGetSession }, cookies }) => {
+export const load: LayoutServerLoad = async ({ locals: { safeGetSession, supabase }, cookies }) => {
 	const { user } = await safeGetSession();
-	return {
-		user,
-		cookies: cookies.getAll()
-	};
+
+	let rol = null;
+	if (user?.id) {
+		rol = await PerfilDB.readRolByIdUsuario(supabase, user.id);
+	}
+
+	return { rol, user, cookies: cookies.getAll() };
 };
