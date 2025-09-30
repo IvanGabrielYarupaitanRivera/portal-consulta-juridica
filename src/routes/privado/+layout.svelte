91482a8a92d9ea57a85f796587403f7ea07b89c6
Ivan <ivangyr321@gmail.com>
2025-09-30 00:11:37 -0500

    Refactor el layout para simplificar la estructura del contenedor principal

--- src/routes/privado/+layout.svelte
+++ src/routes/privado/+layout.svelte
@@ -2,9 +2,9 @@
 	let { children } = $props();
 </script>
 
-<div class=" flex h-screen bg-gray-50">
-	<!-- Main content -->
-	<div class="mx-auto flex max-w-7xl flex-1 flex-col overflow-hidden">
+<!-- Main content -->
+<div class="bg-gray-50">
+	<div class="mx-auto flex max-w-7xl flex-1">
 		<!-- Main content area -->
 		<main class=" flex-1 overflow-auto p-6">
 			{@render children()}
